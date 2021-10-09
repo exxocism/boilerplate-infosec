2b2d787f709a430646f44618ca52c0d9938460fd
Charlie_mac <tchoi.tab@gmail.com>
2021-10-09 18:20:01 +0900

    migrating to windows lol

--- myApp.js
+++ myApp.js
@@ -79,7 +79,7 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 80;
+let port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
